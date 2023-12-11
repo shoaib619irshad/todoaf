3eed1034fba2503ed670b0e58922347ce84cbe41
shoaib619irshad <alqqure@gmail.com>
2023-12-11 18:05:02 +0530

    Forgot Password

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,6 +1,6 @@
 from flask import request , jsonify , abort , Blueprint
-from flask_bcrypt import generate_password_hash , check_password_hash
-from flask_jwt_extended import create_access_token
+from flask_bcrypt import  check_password_hash ,generate_password_hash
+from flask_jwt_extended import create_access_token , get_current_user , jwt_required
 import datetime , os
 from controllers.auth import *
 from flask import current_app as app
@@ -15,14 +15,13 @@ def signup():
         abort(500)
     email = request.json.get("email",None)
     password = request.json.get("password",None)
-    role = request.json.get("role",str(Role.EMPLOYEE))
+    role = request.json.get("role",Role.EMPLOYEE.value)
     is_role_validate = validate_role(role)
     if  not is_role_validate:
         return jsonify(message="Invalid Role"), 500
     validate_email_pass(email,password)
     user = get_user_by_email(email)
     if not user:
-        password = generate_password_hash(password).decode('utf8')
         add_user(email,password,role)
         return jsonify({
             "message":"User Signup Successfully",
@@ -50,3 +49,33 @@ def login():
             return jsonify(access_token=token)
         
     return jsonify(message="The email or password is incorrect")
+
+
+@user_bp.route('/user/forgot_pass' , methods=["POST"])
+def forgot_password():
+    if not request.json:
+        abort(500)
+    email = request.json.get("email",None)
+    if email is None:
+        return jsonify(message="Invalid Request") , 500
+    user = get_user_by_email(email)
+    if user:
+        expires = datetime.timedelta(days=int(os.getenv('DAYS')))
+        token = create_access_token(identity=email ,expires_delta=expires)
+        return jsonify(access_token=token)
+    
+    return jsonify(message="The email is incorrect")
+
+@user_bp.route('/user/reset_pass' , methods=["Patch"])
+@jwt_required()
+def reset_password():
+    if not request.json:
+       abort(500)
+    password = request.json.get("password",None)
+    if password is None:
+        return jsonify(message="Invalid Request") , 500
+    password = generate_password_hash(password).decode('utf8')
+    user = get_current_user()
+    email= user["email"]
+    mongo.db.user.find_one_and_update({'email':email} , {"$set": {"password":password}})
+    return jsonify(message="Password Reset Successfully")
