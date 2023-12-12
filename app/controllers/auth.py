7540c997016ea7049d4feaa3039af20a4321653c
shoaib619irshad <alqqure@gmail.com>
2023-12-12 16:28:03 +0530

    code changed

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -2,6 +2,7 @@ from flask import jsonify
 from flask import current_app as app
 from enum import Enum
 from flask_bcrypt import generate_password_hash
+import base64
 
 
 mongo = app.extensions['mongo']
@@ -23,6 +24,19 @@ def get_user_by_email(email):
 def add_user(email,password,role):
     password = generate_password_hash(password).decode('utf8')
     return mongo.db.user.insert_one({'email': email, 'password': password, "role":role})
+
+def encoder(message):
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
+
+def decoder(base64_message):
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    return message
+
 
 class Role(Enum):
     ADMIN = "admin"
